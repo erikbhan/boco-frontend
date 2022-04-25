d062dcc0219fccc0f34c54c3087aeb91c0d84c4f
Titus Kristiansen <titusk@stud.ntnu.no>
2022-04-25 15:01:40 +0200

    Updated tests

--- tests/unit/apiutil-login-mock.spec.js
+++ tests/unit/apiutil-login-mock.spec.js
@@ -5,42 +5,38 @@ jest.mock("axios");
 
 describe("testing mocking of apiutil.js", () => {
   it("check that login fails with wrong credentials - against mock", async () => {
-    // mock api response on POST call (once)
-    const expectedLoginResponse = "Login failed" ;
 
-    axios.post.mockImplementation(() =>
-      Promise.resolve({data: "Login failed"})
-    );
-
-    // do the call
     const loginRequest = {
       email: "wrong@email.com",
-      password: "thisiswrong123",
-    };
+      password: "thisiswrong123"};
+
+    const expectedLoginResponse = "Login failed";
+
+    axios.post.mockImplementation(() =>
+      Promise.resolve({ data: expectedLoginResponse })
+    );
+
     const loginResponse = await doLogin(loginRequest);
 
-    //  check response
-    //  note that even if wrong username and password are used, mock is configured to return Success
     expect(loginResponse).toEqual(expectedLoginResponse);
   });
   it("check that login succeeds when correct credentials - against mock", async () => {
-    // mock api response on POST call (once)
+
+    const loginRequest = {
+      email: "correct@email.com",
+      password: "thisiscorrect123"};
+
     const apiResponse = {
       response:
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
     };
-    const expectedLoginResponse = "Login failed";
-    axios.post.mockImplementation(() => Promise.resolve({data: apiResponse}));
 
-    // do the call
-    const loginRequest = {
-      email: "correct@email.com",
-      password: "thisiscorrect123",
-    };
+    const expectedLoginResponse = "Login failed";
+
+    axios.post.mockImplementation(() => Promise.resolve({ data: apiResponse }));
+
     const loginResponse = await doLogin(loginRequest);
 
-    //  check response
-    //  note that even if wrong username and password are used, mock is configured to return Success
     expect(loginResponse).not.toEqual(expectedLoginResponse);
   });
 });
