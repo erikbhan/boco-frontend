fb127c67daff9e7f54ff3cfb2e4870cb75d7cfac
Titus Kristiansen <titusk@stud.ntnu.no>
2022-04-27 10:23:11 +0200

    New password form fix and home form renaming

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
-  <HelloWorld />
+  <Home />
 </template>
 
 <script>
 import { defineComponent } from "vue";
 
 // Components
-import HelloWorld from "../components/HelloWorld.vue";
+import Home from "../components/Home.vue";
 
 export default defineComponent({
   name: "HomeView",
 
   components: {
-    HelloWorld,
+    Home,
   },
 });
 </script>
