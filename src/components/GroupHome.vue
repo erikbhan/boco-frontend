494cfd200229fc8370f36dcdd9cd7d8448e4f2e0
Zara Mudassar <zara.1310@hotmail.com>
2022-04-28 08:39:14 +0200

    Error fixed

--- src/components/GroupHome.vue
+++ src/components/GroupHome.vue
@@ -7,21 +7,21 @@
       <span class="absolute inset-y-0 left-0 flex items-center pl-3">
         <svg class="w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none">
           <path
-              d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+            d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
           ></path>
         </svg>
       </span>
 
       <input
-          type="text"
-          id="searchInput"
-          class="w-full py-3 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-          placeholder="Search"
-          v-model="search"
+        type="text"
+        id="searchInput"
+        class="w-full py-3 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+        placeholder="Search"
+        v-model="search"
       />
     </div>
 
@@ -35,12 +35,10 @@
 
 <script>
 import ItemCard from "@/components/ItemCard";
-import LargeProfileCard from "@/components/UserProfileComponents/LargeProfileCard";
 export default {
   name: "SearchItemListComponent",
 
   components: {
-    LargeProfileCard,
     ItemCard,
   },
 
@@ -49,10 +47,10 @@ export default {
       let filteredItems = [];
 
       filteredItems = this.items.filter(
-          (p) =>
-              p.title.toLowerCase().includes(this.search.toLowerCase()) ||
-              p.adresse.toLowerCase().includes(this.search.toLowerCase()) ||
-              p.price === Number(this.search)
+        (p) =>
+          p.title.toLowerCase().includes(this.search.toLowerCase()) ||
+          p.adresse.toLowerCase().includes(this.search.toLowerCase()) ||
+          p.price === Number(this.search)
       );
 
       return filteredItems;
@@ -85,7 +83,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-
-</style>
