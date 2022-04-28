00d865fbc40e119c57e74cf4d473feef0e139810
Zara Mudassar <zara.1310@hotmail.com>
2022-04-28 15:02:57 +0200

    Test for GetListingOfCommunity api method

--- tests/unit/apiutil-communityHome-mock.spec.js
+++ tests/unit/apiutil-communityHome-mock.spec.js
@@ -23,4 +23,36 @@ describe("testing mocking of apiutil.js", () => {
         const communityResponse = await GetCommunity(4040);
         expect(communityResponse.name).toBe(expectedResponse.name);
     });
+
+    it("check that existing group returns correct listings", async () => {
+
+        const expectedResponse = {
+            item1: {
+                title: "Fiskekurs",
+                description: "Fisking og sånn",
+                pricePerDay: 200,
+                address: "Vannet",
+                userID: 6,
+                categoryNames: null,
+                communityIDs: null
+            },
+
+            item2: {
+                title: "TestFraFrontend",
+                description: "oslo",
+                pricePerDay: 500,
+                address: "oslo",
+                userID: 1,
+                categoryNames: null,
+                communityIDs: null
+            },
+        };
+
+        axios.get.mockImplementation(() =>
+            Promise.resolve({ data: expectedResponse })
+        );
+
+        const communityItemResponse = await GetListingsInCommunity(4040);
+        expect(communityItemResponse).toBe(expectedResponse);
+    });
 });
