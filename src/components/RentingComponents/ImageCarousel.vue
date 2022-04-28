09f0f1a8230d2e21492283dc4f59c911f91d84c2
saschrad <saschrad@stud.ntnu.no>
2022-04-28 13:58:49 +0200

    Added image carousel component

--- src/components/RentingComponents/ImageCarousel.vue
+++ src/components/RentingComponents/ImageCarousel.vue
@@ -1,11 +1,11 @@
 <template>
-    <div id="carouselExampleIndicators" class="carousel slide relative" data-bs-ride="carousel">
+    <div id="carouselIndicators" class="carousel slide relative" data-bs-ride="carousel">
         <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
             <button
             v-for="(image, i) in images"
             :key="i"
             type="button"
-            data-bs-target="#carouselExampleIndicators"
+            data-bs-target="#carouselIndicators"
             :data-bs-slide-to="i"
             :class="{'active': i === 0}"
             :aria-current="i === 0 ? 'true' : 'false'"
@@ -24,7 +24,7 @@
         <button
             class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
             type="button"
-            data-bs-target="#carouselExampleIndicators"
+            data-bs-target="#carouselIndicators"
             data-bs-slide="prev"
         >
             <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
@@ -33,7 +33,7 @@
         <button
             class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
             type="button"
-            data-bs-target="#carouselExampleIndicators"
+            data-bs-target="#carouselIndicators"
             data-bs-slide="next"
         >
             <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
