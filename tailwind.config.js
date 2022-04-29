ade82207cb70577fbc55ae332cc6711c6cd18afa
Gilgard <Hjelljord.alida@gmail.com>
2022-04-29 16:05:07 +0200

    theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@ module.exports = {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      white: "#fff",
+      black: "#000",
+      gray: {
+        50: "#f9fafb",
+        100: "#f3f4f6",
+        200: "#e5e7eb",
+        300: "#d1d5db",
+        400: "#9ca3af",
+        500: "#6b7280",
+        600: "#4b5563",
+        700: "#374151",
+        800: "#1f2937",
+        900: "#111827",
+      },
+      primary: {
+        light: "#306EC1",
+        medium: "#004aad",
+        dark: "#003884",
+      },
+      contrast: {
+        light: "#653273",
+        dark: "#731050",
+      },
+    },
   },
   plugins: [],
 };
