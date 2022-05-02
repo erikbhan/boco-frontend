84defc20fef7b124c4d49d3b9dceaec58c37ac3f
Titus Kristiansen <titusk@stud.ntnu.no>
2022-05-02 09:09:49 +0200

    New community using ColoredButton now

--- src/components/CommunityComponents/NewCommunityForm.vue
+++ src/components/CommunityComponents/NewCommunityForm.vue
@@ -175,13 +175,12 @@
 
     <!-- Save item button -->
     <div class="flex justify-center mt-10">
-      <button
+      <Button
         @click="saveClicked"
-        class="content-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         id="saveButton"
+        :text="'Lagre'"
       >
-        Lagre
-      </button>
+      </Button>
     </div>
   </div>
 </template>
@@ -190,10 +189,14 @@
 import useVuelidate from "@vuelidate/core";
 import { required, helpers, maxLength } from "@vuelidate/validators";
 import { postNewgroup } from "@/utils/apiutil";
+import Button from "@/components/BaseComponents/ColoredButton";
 
 export default {
   name: "CreateNewGroup.vue",
 
+  components:{
+    Button,
+  },
   setup() {
     return { v$: useVuelidate() };
   },
