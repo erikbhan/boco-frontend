189763f02056295e95fa2552bf31010feadd1b8c
Erik Borgeteien Hansen <erik@erikbhan.no>
2022-05-02 20:05:35 +0200

    add view for community admin and community admin settings

---
+++ src/components/CommunityComponents/CommunitySettings.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="grid place-content-center h-48">
+    <IconButton
+      @click="deleteCommunity"
+      :buttonColor="'red'"
+      :text="'Slett felleskap'"
+    />
+  </div>
+</template>
+
+<script>
+// import AdminService from "@/services/community-admin.service";
+import IconButton from "@/components/BaseComponents/IconButton.vue";
+
+//TODO: OPEN CONFIRMATION DIALOG WHEN DELETING
+
+export default {
+  components: {
+    IconButton,
+  },
+  methods: {
+    deleteCommunity() {
+      console.log("DELETED");
+      // AdminService.deleteCommunity(this.$route.params.communityID);
+    },
+  },
+};
+</script>
