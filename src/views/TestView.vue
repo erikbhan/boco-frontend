3cf919a850f74dc7600b8785180ff477f8adb9c4
Titus Kristiansen <titusk@stud.ntnu.no>
2022-05-03 09:41:57 +0200

    Rating modal

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,15 +1,26 @@
 <template>
-  <div />
+  <div >
+    <button @click="toggleModal">Hello</button>
+    <RatingModal
+        @close="this.show = false"
+        :visible=show
+        :name="'Test Testesnes'"
+        :title="'Hammer '"
+    ></RatingModal>
+  </div>
 </template>
 
 <script>
+import RatingModal from "@/components/BaseComponents/RatingModal";
 export default {
   data() {
     return {
       show: false,
     };
   },
-  components: {},
+  components: {
+    RatingModal,
+  },
   methods: {
     toggleModal() {
       this.show = !this.show;
