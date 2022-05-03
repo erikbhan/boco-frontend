bdd6a24c99b1f62af9a83141a357e0b42e08d66c
Erik Borgeteien Hansen <erik@erikbhan.no>
2022-05-03 08:14:00 +0200

    remove duplicate css

--- src/components/ItemComponents/NewItemForm.vue
+++ src/components/ItemComponents/NewItemForm.vue
@@ -84,7 +84,7 @@
           <li>
             <div class="form-check" v-for="group in groups" :key="group">
               <input
-                class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-primary-medium checked:bg-primary-medium focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-primary-medium focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
                 type="checkbox"
                 :value="group.communityId"
                 @change="onChangeGroup($event)"
