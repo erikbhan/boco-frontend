680747c24c00dbda737f7f4b84b786bce95cdb01
Titus Kristiansen <titusk@stud.ntnu.no>
2022-05-03 15:54:32 +0200

    Update TestView.vue

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,20 +1,10 @@
 <template>
   <div >
-    <button @click="toggleModal">Hello</button>
-    <RatingModal
-        @close="this.show = false"
-        :visible=show
-        :name="'Test Testesnes'"
-        :title="'Hammer '"
-        :renter-is-receiver-of-rating="true"
-        :rent-i-d="123"
-
-    ></RatingModal>
+  
   </div>
 </template>
 
 <script>
-import RatingModal from "@/components/BaseComponents/RatingModal";
 export default {
   data() {
     return {
@@ -22,7 +12,6 @@ export default {
     };
   },
   components: {
-    RatingModal,
   },
   methods: {
     toggleModal() {
