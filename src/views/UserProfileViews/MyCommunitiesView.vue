859ba61aa20a0d5a2da31b9895bf999ba1ed2870
Gilgard <Hjelljord.alida@gmail.com>
2022-05-04 12:53:47 +0200

    Updated my communities page

---
+++ src/views/UserProfileViews/MyCommunitiesView.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- My communities, with pagination -->
+  <div class="flex flex-row p-4 relative">
+    <div class="text-xl md:text-2xl text-primary-light font-medium w-full">
+      Mine grupper
+    </div>
+    <UserAddIcon
+      class="cursor-pointer max-h-6 max-w-6 float-right grow text-primary-dark"
+      @click="$router.push('/newCommunity')"
+      alt="Opprett ny gruppe"
+    />
+  </div>
+  <CommunityList :communities="myCommunities" :member="true" />
+</template>
+
+<script>
+import CommunityList from "@/components/CommunityComponents/CommunityList.vue";
+import CommunityService from "@/services/community.service";
+import { UserAddIcon } from "@heroicons/vue/outline";
+
+export default {
+  data() {
+    return {
+      myCommunities: [],
+    };
+  },
+  components: {
+    CommunityList,
+    UserAddIcon,
+  },
+  async beforeCreate() {
+    this.myCommunities = await CommunityService.getUserCommunities();
+  },
+};
+</script>
