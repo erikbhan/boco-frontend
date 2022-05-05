632aa5fd6b0e355f0bf3b789b1df8bcca1e75fec
Titus Kristiansen <titusk@stud.ntnu.no>
2022-05-05 09:48:29 +0200

    Footer linted

--- src/components/BaseComponents/FooterBar.vue
+++ src/components/BaseComponents/FooterBar.vue
@@ -1,22 +1,25 @@
 <template>
-  <footer class="w-full bg-white dark:bg-gray-800 sm:flex-row border-1 border-t border-b border-gray-600 h-10">
-
-    <p class="float-left text-xs my-3 ml-4">© BoCo 2022 - All rights reserved</p>
+  <footer
+    class="w-full bg-white dark:bg-gray-800 sm:flex-row border-1 border-t border-b border-gray-600 h-10"
+  >
+    <p class="float-left text-xs my-3 ml-4">
+      © BoCo 2022 - All rights reserved
+    </p>
     <QuestionMarkCircleIcon
-        class="md:mt-0 mt-1 mr-4 float-right cursor-pointer h-8 md:h-10 text-primary-medium"
-        alt="Hjelp"
-        @click="$router.push('/help')"
+      class="md:mt-0 mt-1 mr-4 float-right cursor-pointer h-8 md:h-10 text-primary-medium"
+      alt="Hjelp"
+      @click="$router.push('/help')"
     />
   </footer>
 </template>
 
 <script>
-import { QuestionMarkCircleIcon } from "@heroicons/vue/outline"
+import { QuestionMarkCircleIcon } from "@heroicons/vue/outline";
 export default {
   name: "FooterBar",
 
-  components:{
+  components: {
     QuestionMarkCircleIcon,
   },
-}
+};
 </script>
