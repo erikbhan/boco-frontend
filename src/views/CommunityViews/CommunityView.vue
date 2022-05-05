65c7f81a676e869b22d2f5fc6e8b16173ec72fb0
Håkon Røskaft <haakoero@stud.ntnu.no>
2022-05-05 10:35:16 +0200

    Pulled main

--- src/views/CommunityViews/CommunityView.vue
+++ src/views/CommunityViews/CommunityView.vue
@@ -1,72 +1,72 @@
 <template>
   <!-- My communities, with pagination -->
   <div class="min-h-screen">
-  <div v-if="loggedIn">
-    <div class="flex flex-row p-4 relative">
-      <div class="text-xl md:text-2xl text-primary-light font-medium w-full">
-        Mine grupper
+    <div v-if="loggedIn">
+      <div class="flex flex-row p-4 relative">
+        <div class="text-xl md:text-2xl text-primary-light font-medium w-full">
+          Mine grupper
+        </div>
+        <UserAddIcon
+          class="cursor-pointer max-h-6 max-w-6 float-right grow text-primary-dark"
+          @click="$router.push('/newCommunity')"
+          alt="Opprett ny gruppe"
+        />
       </div>
-      <UserAddIcon
-        class="cursor-pointer max-h-6 max-w-6 float-right grow text-primary-dark"
-        @click="$router.push('/newCommunity')"
-        alt="Opprett ny gruppe"
+      <CommunityList :communities="visibleMyCommunities" :member="true" />
+
+      <!-- pagination my communities -->
+      <div class="flex justify-center">
+        <PaginationTemplate
+          v-bind:items="myCommunities"
+          v-on:page:update="updatePageMyCommunities"
+          v-bind:currentPage="currentPageMyCommunities"
+          v-bind:pageSize="pageSizeMyCommunities"
+          class="mt-4"
+        />
+      </div>
+    </div>
+
+    <!-- Public communities, with search and pagination -->
+    <p class="text-xl md:text-2xl text-primary-light font-medium w-full p-4">
+      Offentlige grupper
+    </p>
+    <!-- Search field -->
+    <div class="relative mt-1 mx-2" id="searchComponent">
+      <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+        <div class="w-5 h-5 text-gray-400">
+          <SearchIcon />
+        </div>
+      </span>
+
+      <input
+        type="text"
+        id="searchInput"
+        class="w-full py-3 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-primary-medium dark:focus:border-primary-medium focus:outline-none focus:ring"
+        placeholder="Search"
+        v-model="search"
+        @change="searchWritten"
       />
     </div>
-    <CommunityList :communities="visibleMyCommunities" :member="true" />
 
-    <!-- pagination my communities -->
+    <!-- Public communities list, two lists, one for when it's searched and one for pagination -->
+    <div v-if="showPaginated">
+      <CommunityList :communities="visiblePublicCommunities" :member="false" />
+    </div>
+    <div v-if="showSearched">
+      <CommunityList :communities="searchPublicCommunities" :member="false" />
+    </div>
+    <!-- pagination Public communities -->
     <div class="flex justify-center">
       <PaginationTemplate
-        v-bind:items="myCommunities"
-        v-on:page:update="updatePageMyCommunities"
-        v-bind:currentPage="currentPageMyCommunities"
-        v-bind:pageSize="pageSizeMyCommunities"
-        class="mt-4"
+        v-bind:items="publicCommunities"
+        v-on:page:update="updatePagePublicCommunities"
+        v-bind:currentPage="currentPagePublicCommunities"
+        v-bind:pageSize="pageSizePublicCommunities"
+        class="my-4"
       />
     </div>
   </div>
-
-  <!-- Public communities, with search and pagination -->
-  <p class="text-xl md:text-2xl text-primary-light font-medium w-full p-4">
-    Offentlige grupper
-  </p>
-  <!-- Search field -->
-  <div class="relative mt-1 mx-2" id="searchComponent">
-    <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-      <div class="w-5 h-5 text-gray-400">
-        <SearchIcon />
-      </div>
-    </span>
-
-    <input
-      type="text"
-      id="searchInput"
-      class="w-full py-3 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-primary-medium dark:focus:border-primary-medium focus:outline-none focus:ring"
-      placeholder="Search"
-      v-model="search"
-      @change="searchWritten"
-    />
-  </div>
-
-  <!-- Public communities list, two lists, one for when it's searched and one for pagination -->
-  <div v-if="showPaginated">
-    <CommunityList :communities="visiblePublicCommunities" :member="false" />
-  </div>
-  <div v-if="showSearched">
-    <CommunityList :communities="searchPublicCommunities" :member="false" />
-  </div>
-  <!-- pagination Public communities -->
-  <div class="flex justify-center">
-    <PaginationTemplate
-      v-bind:items="publicCommunities"
-      v-on:page:update="updatePagePublicCommunities"
-      v-bind:currentPage="currentPagePublicCommunities"
-      v-bind:pageSize="pageSizePublicCommunities"
-      class="my-4"
-    />
-  </div>
-    </div>
-    <FooterBar></FooterBar>
+  <FooterBar></FooterBar>
 </template>
 
 <script>
