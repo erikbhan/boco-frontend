7ff5d674eaa69a4ac38cc047cd89362a33c1be0d
Gilgard <Hjelljord.alida@gmail.com>
2022-05-05 18:49:45 +0200

    lint

--- src/components/UserProfileComponents/UserItems.vue
+++ src/components/UserProfileComponents/UserItems.vue
@@ -58,7 +58,9 @@
             >
               <li>
                 <button
-                  @click="this.$router.push('/item/' + item.listingID + '/edit')"
+                  @click="
+                    this.$router.push('/item/' + item.listingID + '/edit')
+                  "
                   class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                 >
                   Rediger gjenstand
@@ -121,7 +123,9 @@
             >
               <li>
                 <button
-                  @click="this.$router.push('/item/' + item.listingID + '/edit')"
+                  @click="
+                    this.$router.push('/item/' + item.listingID + '/edit')
+                  "
                   class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                 >
                   Rediger gjenstand
