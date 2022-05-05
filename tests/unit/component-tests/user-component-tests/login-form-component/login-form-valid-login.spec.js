2bcc85e397c74465696d6babb18c54e6fc874eb9
XkijuX <sanderhegland@gmail.com>
2022-05-05 19:58:48 +0200

    Added more tests for loginForm

---
+++ tests/unit/component-tests/user-component-tests/login-form-component/login-form-valid-login.spec.js
@@ -0,0 +1,56 @@
+import { shallowMount } from "@vue/test-utils";
+import LoginForm from "@/components/FormComponents/LoginForm.vue";
+import vueRouter from 'vue-router'
+
+jest.mock('@/utils/apiutil', () => {
+    return {
+        doLogin: () => {
+            return new Promise((resolve) => {
+                resolve({
+                    isLoggedIn: true
+                });
+            })
+        }
+    }
+});
+
+describe("LoginForm component", () => {
+    let wrapper;
+    const mockRouter = {
+        push: jest.fn()
+    }
+    const mockStore = {
+        commit: jest.fn()
+    }
+    
+    beforeEach(() => {
+        wrapper = shallowMount(LoginForm, {
+            global: {
+                mocks: {
+                    $router: mockRouter,
+                    $store: mockStore
+                },
+            }
+        });
+    });
+
+    it("Check valid login", async () => {
+            
+        // Verify that the error message is empty
+        expect(wrapper.vm.message).toBe("");
+        const field = wrapper.findAll("input")[0];
+        field.setValue("test@test.com");
+        const field2 = wrapper.findAll("input")[1];
+        field2.setValue("testtest");
+
+        // Click on the login button
+        const button = wrapper.find(".login");
+        button.trigger("click");
+
+        // wait a tick
+        await wrapper.vm.$nextTick();
+        
+        // Check that the error message is not empty
+        expect(mockRouter.push).toBeCalledTimes(1);
+    })
+})
