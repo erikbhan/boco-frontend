e3f327aeaa2afe4c6b1a241cbf4cf41d036d178b
Håkon Røskaft <haakoero@stud.ntnu.no>
2022-05-06 15:48:15 +0200

    Fixed so rating now displayes properly on user profile

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -39,6 +39,28 @@ class UserService {
       .catch((err) => console.error(err));
   }
 
+  async getUserRatingAverageOwner(userId) {
+    return await axios
+      .get(API_URL + "rating/" + userId + "/average/owner", {
+        headers: tokenHeader(),
+      })
+      .then((res) => {
+        return res.data;
+      })
+      .catch((err) => console.error(err));
+  }
+
+  async getUserRatingAverageRenter(userId) {
+    return await axios
+      .get(API_URL + "rating/" + userId + "/average/renter", {
+        headers: tokenHeader(),
+      })
+      .then((res) => {
+        return res.data;
+      })
+      .catch((err) => console.error(err));
+  }
+
   async setListingToDeleted(listingId) {
     return await axios
       .delete(API_URL + "listing/" + listingId, {
